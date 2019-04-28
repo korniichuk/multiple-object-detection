cf116d598f0f91fd51386def280b147aaaf09736
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2019-04-28 15:22:41 +0200

    [m-o-d] Add template of multiple_objects_detection() function

--- multiple_object_detection.py
+++ multiple_object_detection.py
@@ -1,2 +1,32 @@
 # -*- coding: utf-8 -*-
-# Version: 0.1a1
+# Version: 0.1a2
+
+def multiple_objects_detection(template, image, scale=1.0,
+        method='cv.TM_CCOEFF_NORMED', threshold=0.7, mode='hide'):
+    """Multiple object detection. Function compares template against overlapped
+       source image regions.
+    Input:
+        template -- template. Template must be not greater than the source
+                    image and have the same data type (required | type: str);
+        image -- source image. Source image must be 8-bit or 32-bit
+                 floating-point (required | type: str);
+        scale -- scale between source image and template. For example 0.5 scale
+                 means proportional ratio of source image to template as 1:2
+                 (not required | type: float | default: 1.0);
+        method -- template matching method of OpenCV Python library
+                  (not required | type: str | default: 'cv.TM_CCOEFF_NORMED');
+
+        threshold -- value of thresholding. For example 0.7 threshold means
+                     with best matches from 0.7 to 1.0 (>=0.7 and <=1.0);
+        mode -- visualization mode. No visualization in 'hide' mode.
+                Visualization in Matplotlib with 'matplotlib' mode.
+                Visualization in OpenCV with 'opencv' mode (not required |
+                type: str | default: 'hide').
+    Output:
+        result -- coordinates of top left and bottom right points of matched
+                  template. Example [[(1, 1), (2, 2)], [(3, 1), (4, 2)]]
+                  (type: list);
+
+    """
+
+    pass
